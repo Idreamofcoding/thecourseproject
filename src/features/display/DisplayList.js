e6fc11ffc3274e48bb461db7737eaf0065c17f9f
albertrocha <rocharocharocha95@yahoo.com>
2022-07-28 21:18:27 -0400

    added contact form day thursday

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,5 +1,6 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+// import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard.js';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
@@ -12,9 +13,11 @@ const DisplayList = () => {
             {
                 items.map((item, idx) => {
                     return (
-                        <Col md className='m-1' key={idx}>
-                            <DisplayCard item={item} />
-                        </Col>
+                        item && (
+                            <Col md className='m-1' key={idx}>
+                                <AnimatedDisplayCard item={item} />
+                            </Col>
+                        )
                     )
                 })
             }
--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -19,4 +19,3 @@ const PartnersList = () => {
 }
 
 export default PartnersList
-
